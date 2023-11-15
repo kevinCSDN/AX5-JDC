7b307c50ddce80c3bf6d8658fca290e294d2f72e
kevinCSDN <81788909+kevinCSDN@users.noreply.github.com>
2023-11-15 22:05:15 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,4 +17,4 @@
 echo 'src-git helloworld https://github.com/fw876/helloworld.git' >>feeds.conf.default
 echo 'src-git openclash https://github.com/vernesong/OpenClash.git' >>feeds.conf.default
 echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git' >>feeds.conf.default
-echo 'src-git passwall_luci     https://github.com/xiaorouji/openwrt-passwall.git;luci' >>feeds.conf.default
+echo 'src-git passwall_luci     https://github.com/xiaorouji/openwrt-passwall.git' >>feeds.conf.default
